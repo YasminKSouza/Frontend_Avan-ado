26d3fcf942759bcd6b95f151e2cbcc4deaac0bf9
Yasmin Katharine <yasmin_katharine@hotmail.com>
2024-08-28 18:59:15 -0300

    first commit

---
+++ aula1/src/components/PrecisoEstudar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrecisoEstudar = (props) => {
+  return (
+    <div>
+        <h1> Preciso estudar {props.course}</h1>
+    </div>
+  )
+}
+
+export default PrecisoEstudar
