df8b65410fbbdef84772c10f8abe2c135fdacd9d
Yasmin Katharine <yasmin_katharine@hotmail.com>
2024-12-03 17:54:23 -0300

    Adicionando arquivos não rastreados ao repositório

---
+++ aula04/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Exercicio1 from './Components/Exercicio01'
+import Exercicio2 from './Components/Exercicio2'
+import Campanha from './Components/Campanha'
+import FormularioDeContato from './Components/FormularioDeContato'
+
+const App = () => {
+  return (
+    <div>
+      {/*<Exercicio1 */}
+      {/*<Exercicio2 */}
+      {/*<Campanha mes = "novembro" />*/}
+      <FormularioDeContato/>
+    </div>
+  )
+}
+
+export default App
